d82efb0169738d18b0bd061d930bee79c6cfa0c0
enzotorrs <enzotorres969@gmail.com>
2022-10-02 08:22:55 -0300

    feat: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import style from './Header.module.scss'
+
+
+export function Header(){
+    return (
+        <header>
+            <h1 className={style.title}>Pandora Files</h1>
+        </header>
+    )
+}
