08b11dd21364a775dcbd94a69bc78a63d9d84cd1
Enzo Torres <enzotorres969@gmail.com>
2023-01-25 21:46:34 -0300

    feat: add responsive error on fileSize input

--- src/pages/Home/Form/utils/calculateFileSize.ts
+++ src/pages/Home/Form/utils/calculateFileSize.ts
@@ -1,5 +1,5 @@
 export function calculateFileSize(size: string, multiplier: string): number {
-    if(multiplier === ''){
+    if(multiplier === '' || size === ''){
         return 1
     }
     const calculatedFileSize = Number(size) * Number(multiplier)
